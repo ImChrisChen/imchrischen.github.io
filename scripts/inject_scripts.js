7eb28e186ee019150180ba5f3ec49c793f325bff
imchrisorz <imchrisorz@gmail.com>
2025-02-09 23:13:13 +0800

    feat: add google ads

---
+++ scripts/inject_scripts.js
@@ -0,0 +1,4 @@
+hexo.extend.injector.register('head_end', `
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3068336687184631"
+     crossorigin="anonymous"></script>
+`, 'default');
